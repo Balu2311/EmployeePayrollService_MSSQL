91ed840bf4b4c1d2523faa13eeb4edfcaa91975e
Vallem Balu <vbnreddy5@gmail.com>
2021-08-12 14:11:42 +0530

    Alter and Update Table Data

---
+++ AlterTable.sql
@@ -0,0 +1,7 @@
+﻿alter table [dbo].[employee_payroll] add gender char(1)
+
+update [dbo].[employee_payroll] set gender = 'M' where name = 'Reddy' or name = 'Balu'
+update [dbo].[employee_payroll] set gender = 'M' where name = 'Rishi' or name = 'Nagireddy'
+update [dbo].[employee_payroll] set gender = 'F' where name = 'Bhanu' or name= 'Padma'
+
+select * from employee_payroll
